cc83ef21755b2f5ef3b70c5cef94a86c61239c23
Jakub Jirutka <jakub@jirutka.cz>
2017-09-18 19:45:20 +0200

    Add docinfo processor for embedding Rouge CSS

--- lib/asciidoctor/rouge.rb
+++ lib/asciidoctor/rouge.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 require 'asciidoctor/extensions'
 require 'asciidoctor/rouge/version'
+require 'asciidoctor/rouge/docinfo_processor'
 require 'asciidoctor/rouge/treeprocessor'
 
 Asciidoctor::Extensions.register do
   treeprocessor Asciidoctor::Rouge::Treeprocessor
+  docinfo_processor Asciidoctor::Rouge::DocinfoProcessor
 end
---
+++ lib/asciidoctor/rouge/docinfo_processor.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+require 'asciidoctor/rouge/version'
+require 'asciidoctor/extensions'
+require 'rouge'
+
+module Asciidoctor::Rouge
+  # A docinfo processor that embeds CSS for Rouge into the document's header.
+  class DocinfoProcessor < ::Asciidoctor::Extensions::DocinfoProcessor
+
+    # @param document [Asciidoctor::Document] the document to process.
+    # @return [String, nil]
+    def process(document)
+      return unless document.attr?('source-highlighter', 'rouge')
+      return unless document.attr('rouge-css', 'class') == 'class'
+
+      if (theme = ::Rouge::Theme.find(document.attr('rouge-theme', 'github')))
+        css = theme.render(scope: '.highlight')
+        ['<style>', css, '</style>'].join("\n")
+      end
+    end
+  end
+end
